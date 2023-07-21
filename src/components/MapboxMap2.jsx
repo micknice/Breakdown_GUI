9dd7c80fcb11b4fb45155c2af10c5e52129f2f7b
mick nice <mikemccullagh@outlook.com>
2023-07-21 20:50:01 +0100

    adds start simulation from gui

--- src/components/MapboxMap2.jsx
+++ src/components/MapboxMap2.jsx
@@ -10,7 +10,7 @@ const MAPBOX_TOKEN = 'pk.eyJ1IjoiYWlyZm9yY2UyaGlnaCIsImEiOiJjbGtiYTZ4d2wwYXFnM2J
 function MapboxMap2() {
   const [viewState, setViewState] = useState({
     width: '100%',
-    height: '1200px',
+    height: '100%',
     latitude: 54.500000, 
     longitude: -4.2000, 
     zoom: 4.7, 
@@ -18,7 +18,7 @@ function MapboxMap2() {
   const [patrols, setPatrols] = useState({});
   const [markers, setMarkers] = useState([]); 
   const [patData, setPatData] = useState([]);
-
+  
   useEffect(() => {
     const socket = io('http://localhost:7071');
     socket.on('patrolData', (data) => {
@@ -35,7 +35,12 @@ function MapboxMap2() {
     updateMapMarkers();
   }, [patrols]); 
 
- 
+  const handleStartSimulation = () => {
+    console.log('handler invoked')
+    const socket = io('http://localhost:7071');
+    socket.emit('sim', '!!!!!')
+    
+  }
   const updateMapMarkers = () => {
     console.log('updateMapMarkers invoked', Date.now());
 
@@ -59,41 +64,54 @@ function MapboxMap2() {
   };
 
   return (
-    <ReactMapGL
-      {...viewState}
-      onMove={(evt) => setViewState(evt.viewState)}
-      style={{ width: 800, height: 600 }}
-      mapStyle="mapbox://styles/mapbox/streets-v9"
-      mapboxAccessToken={MAPBOX_TOKEN}
-    >
-      {/* Render the markers on the map */}
-      {markers.map((marker, index) => {
-        const { color, latitude, longitude } = marker;
-        console.log(color)
-        const patrolId = patData[index].patrolId
-        return (
-          <Marker key={index} latitude={latitude} longitude={longitude}>
-            <div style={{ color }}>
-              <svg
-                height="20"
-                viewBox="0 0 24 24"
-                style={{
-                  cursor: 'pointer',
-                  fill: color,
-                  stroke: 'none',
-                  transform: `translate(${0 / 2}px,${0}px)`,
-                }}
-                >
-                <circle cx="12" cy="12" r="10" />
-              </svg>
-                
+    <div style={{ width: '100%', height: '100vh', display: 'flex' }}>
+      
+      <div style={{ position: 'absolute', top: 0, left: 0, right: 0, zIndex: 1, padding: '20px' }}>
+        {/* Add your text content here */}
+        <h1>AA SIMULATOR</h1>
+        <p>Break it on down!!!</p>
+        <button onClick={handleStartSimulation}>Start Simulation</button>
+      </div>
+      <div style={{ position: 'absolute', top: 0, bottom: '0', left: '0', transform: 'translateX(-50%)', zIndex: 1 }}>
+        {/* Add your button here */}
+      </div>
+
+      <ReactMapGL
+        {...viewState}
+        onMove={(evt) => setViewState(evt.viewState)}
+        // style={{ width: 2380, height: 1200 }}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxAccessToken={MAPBOX_TOKEN}
+        >
+        {/* Render the markers on the map */}
+        {markers.map((marker, index) => {
+          const { color, latitude, longitude } = marker;
+          console.log(color)
+          const patrolId = patData[index].patrolId
+          return (
+            <Marker key={index} latitude={latitude} longitude={longitude}>
+              <div style={{ color }}>
+                <svg
+                  height="20"
+                  viewBox="0 0 24 24"
+                  style={{
+                    cursor: 'pointer',
+                    fill: color,
+                    stroke: 'none',
+                    transform: `translate(${0 / 2}px,${0}px)`,
+                  }}
+                  >
+                  <circle cx="12" cy="12" r="10" />
+                </svg>
                   
-            </div>
-            
-          </Marker>
-        );
-      })}
-    </ReactMapGL>
+                    
+              </div>
+              
+            </Marker>
+          );
+        })}
+      </ReactMapGL>
+    </div>
   );
 }
 
