5d36373f4f415bb9fb1e2e4a7577a8a5fd3a7fea
mick nice <mikemccullagh@outlook.com>
2023-07-21 10:16:49 +0100

    latest for soran

--- src/components/MapboxMap2.jsx
+++ src/components/MapboxMap2.jsx
@@ -7,7 +7,7 @@ import mapboxgl from 'mapbox-gl';
 
 const MAPBOX_TOKEN = 'pk.eyJ1IjoiYWlyZm9yY2UyaGlnaCIsImEiOiJjbGtiYTZ4d2wwYXFnM2JvMHBvcXQ4dWJhIn0.hWAcsZ9TJm7MzibOfoMXDw';
 
-function MapboxMap2(){
+function MapboxMap2() {
   const [viewState, setViewState] = useState({
     width: '100%',
     height: '1200px',
@@ -16,15 +16,15 @@ function MapboxMap2(){
     zoom: 4.7, // Adjust the initial zoom level as needed
   });
   const [patrols, setPatrols] = useState({});
-  const [markers, setMarkers] = useState([]);
+  const [markers, setMarkers] = useState([]); // Modify initial state to an empty array
   const [patData, setPatData] = useState([]);
 
   useEffect(() => {
     const socket = io('http://localhost:7071');
     socket.on('patrolData', (data) => {
       console.log('Received updated patrols data:', data, Date.now());
-      console.log(data)
-      setPatrols(data); 
+      console.log(data);
+      setPatrols(data);
     });
     return () => {
       socket.disconnect();
@@ -33,60 +33,48 @@ function MapboxMap2(){
 
   useEffect(() => {
     updateMapMarkers();
-  }, [patrols]);
-
-  
+  }, [patrols, patData]); // Add patData to the dependency array
 
   const updateMapMarkers = () => {
-    console.log('updateMapMarkers invoked', Date.now())
-    
-    // // console.log('patrols!!!', patrols.patrol0.currentLocation)
-    const patrolArr = Object.values(patrols);
-    // console.log('patrolArr.length', patrolArr.length)
-    if(patrolArr.length > 0) {
-      const markerArr = extractLatitudeAndLongitude(patrolArr, markers)
-    
-      setPatData(patrolArr);
-      setMarkers(markerArr);
-  
-      
-    }
-    // Then you can update the markers state with the new marker data.
+    console.log('updateMapMarkers invoked', Date.now());
 
+    const patrolArr = Object.values(patrols);
+    if (patrolArr.length > 0) {
+      const markerArr = extractLatitudeAndLongitude(patrolArr, markers).map((marker, index) => {
+
+        const onJob = patrolArr[index].onJob;
+
+
+        const color = onJob ? 'red' : 'green';
+
+ 
+        return { ...marker, color };
+      });
+
+ 
+      setMarkers(markerArr);
+      setPatData(patrolArr);
+    }
   };
 
   return (
-    <ReactMapGL {...viewState} 
-    onMove={evt => setViewState(evt.viewState)}
-    style={{width: 800, height: 600}}
-    mapStyle="mapbox://styles/mapbox/streets-v9"
-    mapboxAccessToken={MAPBOX_TOKEN}>
+    <ReactMapGL
+      {...viewState}
+      onMove={(evt) => setViewState(evt.viewState)}
+      style={{ width: 800, height: 600 }}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+      mapboxAccessToken={MAPBOX_TOKEN}
+    >
       {/* Render the markers on the map */}
-      {/* Example: */}
       {markers.map((marker, index) => {
-        // if(patData[index].onJob) {
-          
-          const popup = new mapboxgl.Popup().setHTML(`<h1>${patData[index].patrolId}</h1>`)
-          return(         
-            <Marker key={index} latitude={marker.latitude} longitude={marker.longitude} color="red" popup={popup}/>)
-        // } else {
-        //   return (
-        //     <Marker key={index} latitude={marker.latitude} longitude={marker.longitude} color="green"/>)
-          
-        // }
-        
-          
-             
-        
+        const { color, latitude, longitude } = marker;
+        const popup = new mapboxgl.Popup().setHTML(`<h1>${patData[index].patrolId}</h1>`);
+        return (
+          <Marker key={index} latitude={latitude} longitude={longitude} color={color} popup={popup} />
+        );
       })}
-        {/* <Marker key={index} longitude={marker.longitude} latitude={marker.latitude} color="red" /> */}
-        {/* <Marker longitude={-2.936226} latitude={50.933313} color="red" />
-        <Marker key={'any'} latitude={50.933313} longitude={-2.936226}>
-        <img alt=''src="./pin_ting.png" />
-        </Marker> */}
-      
     </ReactMapGL>
   );
-};
+}
 
 export default MapboxMap2;
